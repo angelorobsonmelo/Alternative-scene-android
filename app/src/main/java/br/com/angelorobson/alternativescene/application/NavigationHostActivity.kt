7543cdd60cfbca0d30f675689fa90eb95239c836
angelomelo <angelorobsonmelo@gmail.com>
2020-02-27 17:04:50 -0300

    implementing deep link

--- app/src/main/java/br/com/angelorobson/alternativescene/application/NavigationHostActivity.kt
+++ app/src/main/java/br/com/angelorobson/alternativescene/application/NavigationHostActivity.kt
@@ -1,6 +1,7 @@
 package br.com.angelorobson.alternativescene.application
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -12,7 +13,9 @@ import br.com.angelorobson.alternativescene.R
 import br.com.angelorobson.alternativescene.application.commom.utils.Constants.EventsContants.DETAIL_EVENT_REQUEST_CODE
 import br.com.angelorobson.alternativescene.application.commom.utils.extensions.notEqual
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import kotlinx.android.synthetic.main.host_navigation_activity.*
+import java.text.MessageFormat
 
 
 class NavigationHostActivity : AppCompatActivity() {
@@ -27,6 +30,40 @@ class NavigationHostActivity : AppCompatActivity() {
         bottomNavigation?.setupWithNavController(navController)
 
         mBottomNavigationView = bottomNavigation
+
+
+        FirebaseDynamicLinks.getInstance()
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                // Get deep link from result (may be null if no link is found)
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deepLink = pendingDynamicLinkData.link
+                }
+
+                // Handle the deep link. For example, open the linked
+                // content, or apply promotional credit to the user's
+                // account.
+                // ...
+
+                // ...
+            }
+            .addOnFailureListener(this) { e -> Log.w("TAG", "getDynamicLink:onFailure", e) }
+
+        val id = 166
+        val firebaseLink = "https://angelorobsonn.page.link"
+        val myLink = "https://www.angelorobson.com?id=$id"
+        val myPackage = "br.com.angelorobson.alternativescene"
+        val link = "{0}?link={1}&apn={2}"
+    }
+
+    private fun shareDeepLink(deepLink: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Firebase Deep Link")
+        intent.putExtra(Intent.EXTRA_TEXT, deepLink)
+
+        startActivity(intent)
     }
 
     override fun onSupportNavigateUp(): Boolean =
